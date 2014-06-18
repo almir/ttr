d9c74d5f7717f4137ddce4b074af80b00ce40d95
almir <almir@dzinovic.net>
2014-06-18 21:49:08 +0200

    Update copyright year.

--- src/net/geniecode/ttr/ScheduleReceiver.java
+++ src/net/geniecode/ttr/ScheduleReceiver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 GenieCode
+ * Copyright (C) 2013-2014 GenieCode
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
